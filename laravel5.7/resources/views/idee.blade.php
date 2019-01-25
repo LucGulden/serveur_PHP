af882202b38e5ac06b80b2948b70ba9e739c30ee
dylanLafarge <38322888+dylanLafarge@users.noreply.github.com>
2019-01-25 17:22:40 +0100

    Système de like fonctionnel

--- laravel5.7/resources/views/idee.blade.php
+++ laravel5.7/resources/views/idee.blade.php
@@ -16,28 +16,12 @@
        		<label for="descriptionidee">Description de votre idée:</label>
        		<textarea name="descriptionidee" id="descriptionidee"></textarea>
 		   </p>
-		   <p id="soumettre"> <button type="submit" onclick = "submitform()">Soumettre l'idée!</button> </p>
+		   <p id="soumettre"> <button type="submit" onclick = "soumettreIdee()">Soumettre l'idée!</button> </p>
     </div>
 
 </section>
 
 <section id = "testget">
-    <div class="container-fluid border border-warning rounded mb-0">
-        <h3>Lorem ipsum dolor sit amet</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vehicula vehicula sapien, in accumsan ipsum gravida a. Integer scelerisque, felis sed ultricies lobortis, justo quam aliquet est, id pretium ante odio eu orci. In sit amet mauris et risus suscipit vehicula vel a justo. Sed velit tellus, faucibus in magna ac, faucibus posuere enim. Mauris nec tortor neque. Mauris non nulla urna. Maecenas ut risus ac odio tincidunt imperdiet pharetra quis nunc. Nunc in laoreet libero. Fusce lobortis dolor sit amet ultricies sodales. Quisque laoreet massa a urna aliquet laoreet. Proin sed aliquet neque, sed ultrices massa. Vivamus aliquam lacinia eros in condimentum. Sed lobortis molestie tellus eu euismod. Fusce dignissim blandit felis, id finibus mi sollicitudin eu. Aenean venenatis nulla quis finibus faucibus. Duis in risus et justo maximus laoreet et ut enim.</p>
-        <div class="jaime">    
-        <span class="input-group-btn"><button type="submit">J'aime!</button></span>
-        </div>
-    </div>
-
-    <div class="container-fluid border border-warning rounded mb-0">
-        <h3>Lorem ipsum dolor sit amet</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vehicula vehicula sapien, in accumsan ipsum gravida a. Integer scelerisque, felis sed ultricies lobortis, justo quam aliquet est, id pretium ante odio eu orci. In sit amet mauris et risus suscipit vehicula vel a justo. Sed velit tellus, faucibus in magna ac, faucibus posuere enim. Mauris nec tortor neque. Mauris non nulla urna. Maecenas ut risus ac odio tincidunt imperdiet pharetra quis nunc. Nunc in laoreet libero. Fusce lobortis dolor sit amet ultricies sodales. Quisque laoreet massa a urna aliquet laoreet. Proin sed aliquet neque, sed ultrices massa. Vivamus aliquam lacinia eros in condimentum. Sed lobortis molestie tellus eu euismod. Fusce dignissim blandit felis, id finibus mi sollicitudin eu. Aenean venenatis nulla quis finibus faucibus. Duis in risus et justo maximus laoreet et ut enim.</p>
-        <div class="jaime">    
-        <span class="input-group-btn"><button type="submit">J'aime!</button></span>
-        </div>
-    </div>
-
     <div class="container-fluid border border-warning rounded mb-0">
         <h3>Lorem ipsum dolor sit amet</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vehicula vehicula sapien, in accumsan ipsum gravida a. Integer scelerisque, felis sed ultricies lobortis, justo quam aliquet est, id pretium ante odio eu orci. In sit amet mauris et risus suscipit vehicula vel a justo. Sed velit tellus, faucibus in magna ac, faucibus posuere enim. Mauris nec tortor neque. Mauris non nulla urna. Maecenas ut risus ac odio tincidunt imperdiet pharetra quis nunc. Nunc in laoreet libero. Fusce lobortis dolor sit amet ultricies sodales. Quisque laoreet massa a urna aliquet laoreet. Proin sed aliquet neque, sed ultrices massa. Vivamus aliquam lacinia eros in condimentum. Sed lobortis molestie tellus eu euismod. Fusce dignissim blandit felis, id finibus mi sollicitudin eu. Aenean venenatis nulla quis finibus faucibus. Duis in risus et justo maximus laoreet et ut enim.</p>
@@ -75,7 +59,7 @@ var ideablock = "";
 const getIdea= function(data){
     const taille = data.length - 1;
     for (var i = taille; i >= 0; i--) {
-        ideablock = ideablock + "<div class='container-fluid border border-warning rounded mb-0'> <h3>" + data[i].titre_idee + "</h3> <p>" + data[i].description_idee + "</p> <div class='jaime'> <span class='input-group-btn'><button type='submit'>J'aime!</button></span></div> </div>";
+        ideablock = ideablock + "<div class='container-fluid border border-warning rounded mb-0'> <h3>" + data[i].titre_idee + "</h3> <p>" + data[i].description_idee + "</p> <div class='jaime'> <span class='input-group-btn'><button type='submit' id='" + data[i].id_idee + "' onclick='aimeIdee(this.id)'>J'aime! " + data[i].id_idee + "</button></span></div> </div>";
         document.getElementById("testget").innerHTML = ideablock;
     }
 };
@@ -86,7 +70,7 @@ getJSON('http://localhost:3000/idee/').then(getIdea);
 <!-- POST -->
 <script>
    
-function submitform(){
+function soumettreIdee(){
   
   alert("test");
 
@@ -109,5 +93,28 @@ function submitform(){
         contentType : "application/json"
     });
  }
+ </script>
+
+<!-- J'aime -->
+ <script>
+        
+    function aimeIdee(id) {
+        
+        var datapost = {
+            "id_idee": id,
+            "id_users": 1
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:3000/aime/",
+            data: JSON.stringify(datapost),
+            success: function(){
+                location.reload(true);
+            },
+            dataType: "json",
+            contentType : "application/json"
+        });
+    }       
  </script>
 @endsection
