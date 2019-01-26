3672e92ff958992659ddacde0dc26bf40a34c307
LucGulden <33136081+LucGulden@users.noreply.github.com>
2019-01-26 16:23:41 +0100

    Panier connecté à la bdd

--- laravel5.7/app/Http/Controllers/PanierController.php
+++ laravel5.7/app/Http/Controllers/PanierController.php
@@ -4,18 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Article;
+use Illuminate\Support\Facades\DB;
 
 class PanierController extends Controller
 {
     public function MonPanier()
     {
         
-        $articles = Article::get()
-        ->join('contient','article.id_article','=','contient.id_article')
-        ->join('commande','contient.id_commande','=','commande.id_commande')
-        ->join('achete','commande.id_commande','=','achete.id_commande')
-        ->where('achete.id_user',1)
-        ->where('commande.achevement_commande',0);
+        $articles = DB::connection('mysql')->table('users')
+        ->join('achete','achete.id_users','users.id_users')
+        ->join('commande','commande.id_commande','achete.id_commande')
+        ->join('contient','contient.id_commande','commande.id_commande')
+        ->join('articles','articles.id_article','contient.id_article')
+        ->where('users.id_users',1)
+        ->where('commande.achevement_commande',0)
+        ->get();
 
 
         return view('panier', [
